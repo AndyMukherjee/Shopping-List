aceea914151649b4d5cb18a0c5a8d3f14ffa15ff
Deepanjan Roy <deepanjan.roy@gmail.com>
2015-03-03 17:16:18 -0500

    test

---
+++ shop.js
@@ -0,0 +1,20 @@
+$document .ready(function(){
+
+$(".main").mouseenter(function(){
+
+	alert("hi");
+});
+
+
+/*$("input") 
+	.keyup (function() {
+
+		var value = $(this).val();
+		$("li").text(value);
+
+	})
+	.keyup();*/
+
+});
+
+	
