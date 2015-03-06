966bab85f89094bf69d35f78fb87a99e99d5d1e2
Deepanjan Roy <deepanjan.roy@gmail.com>
2015-03-05 21:19:41 -0500

    test

--- shop.js
+++ shop.js
@@ -3,26 +3,31 @@ $(document) .ready(function(){
 
 
 
-$('.input') 
-	.keyup (function() {
 
-		var value = $(this).val();
-		$('li').text(value);
-
-
-	});
-
-	keyup();
 	$('.button') .click(function(){
-		$("input").val("");
+		var value = $(".input").val();
+
+		$('#shoplist').append("<li><input type='checkbox'>"+value+"<button class='button2'>Remove</button></li>");
+		$(".input").val("");
 
 	});
 
 
 $('ul').on('click', 'li', function(event){
 
+	
+$(this).css("text-decoration","line-through");
+
+
 	});
 
+$('ul').on('click', '.button2', function(){
+
+	$(this).parent().remove();
 });
+
+
+});
+
 
 	
